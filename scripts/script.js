de68a59a3dc74615cd942068bfa3d4a77ffdf29c
codetheke <161771599+codetheke@users.noreply.github.com>
2024-02-29 14:25:59 -0800

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+//......................................................
+//                     CONTACT BTN
+// .....................................................
+document.getElementById("btn-confirm").onclick = function () {
+  window.open("https://wa.me/11234567890", "_blank");
+};
+
+document.getElementById("btn-place").onclick = function () {
+  window.open("https://maps.app.goo.gl/kMeX6GWRnuzPtmST6", "_blank");
+};
+
